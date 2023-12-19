012e55637b480bdd3870d1485f600ad0f85ec2b9
SkylerBanana <Skyler_Banana@protonmail.com>
2023-12-19 10:21:18 -0500

    First Commit

---
+++ chatApp/src/App.jsx
@@ -0,0 +1,11 @@
+import FriendList from "./Components/FriendList";
+function App() {
+  return (
+    <div>
+      <h1>Banana</h1>
+      <FriendList />
+    </div>
+  );
+}
+
+export default App;
---
+++ chatApp/src/Components/FriendList.jsx
@@ -0,0 +1,9 @@
+function FriendList() {
+  return (
+    <div>
+      <img src="https://static.vecteezy.com/system/resources/previews/016/093/189/original/cute-sphynx-cat-cartoon-sitting-vector.jpg"></img>
+    </div>
+  );
+}
+
+export default FriendList;
